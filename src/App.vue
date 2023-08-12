eedf030e2188d0d6a7f16e30525ee20d44bde264
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-08-13 00:29:56 +0700

    add navbar,routing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container py-6 flex flex-col sm:flex-row items-center gap-4 text-white"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-cloud text-2xl"></i>
+          <span class="text-2xl">VVueather</span>
+        </div>
+      </RouterLink>
+      <div class="flex flex-1 gap-3 justify-end">
+        <i
+          class="fa-solid fa-info hover:text-weather-secondary text-xl cursor-pointer duration-200"
+        ></i>
+        <i
+          class="fa-solid fa-plus hover:text-weather-secondary text-xl cursor-pointer duration-200"
+        ></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
