26bcbc7732014f2e91bd796a4217e6115f08625d
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-08-12 23:44:08 +0700

    setup vue tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#2c3e50",
+        "weather-secondary": "#bdc3c7",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
